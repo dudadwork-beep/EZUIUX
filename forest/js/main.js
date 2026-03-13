0e01f958b3e0bc375f843ac0f551faaf5717ee02
dudadwork-beep <dudad.work@gmail.com>
2026-03-13 16:56:02 +0900

    ...

--- forest/js/main.js
+++ forest/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 			if(destination.index == 0){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
 				// console.log('1번째 슬라이드가 로딩 되었을때v'); - ' ' 아무것도 없어도 따옴표 있어야 함.
 				$('.sec_nav').attr('data-bg','')
-				$('.header').attr('data-bg','')
+				$('.header').attr('data-bg','ㅡㅐ')
 			}else if(destination.index == 1){
 				// console.log('2번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg','white')
@@ -29,7 +29,7 @@ $(document).ready(function(){
 			}else if(destination.index == 2){
 				//console.log('3번째 슬라이드가 로딩 되었을때v');
 				$('.sec_nav').attr('data-bg','')
-				$('.header').attr('data-bg','')
+				$('.header').attr('data-bg','mo')
 			}else if(destination.index == 3){
 				// console.log('4번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'white')
@@ -72,5 +72,26 @@ $(document).ready(function(){
 			});
 			/* visual의 swiper 끝*/
 
-
+			/* biz의 swifer*/
+			const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+				slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+				spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+				breakpoints: {
+					450: {    /* 640px 이상일때 적용 */
+						slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+						spaceBetween: 16,
+					},
+					768: {    /* 640px 이상일때 적용 */
+						slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+						spaceBetween: 24,
+					},
+					1280: {    /* 640px 이상일때 적용 */
+						slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+						spaceBetween: 24,
+					},
+				},
+				//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+				loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+				});
+				
 })//$(document).ready
