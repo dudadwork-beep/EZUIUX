94486a09ae5951b30132c0701a59b7daaaabfd84
dudadwork-beep <dudad.work@gmail.com>
2026-03-24 17:07:19 +0900

    ..

--- happybakery/js/main.js
+++ happybakery/js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
         //     delay: 2500,
         //     disableOnInteraction: true,
         // },
-        //effect: "fade", /* fade 효과 */
+        // effect: "fade", /* fade 효과 */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
 	
@@ -28,13 +28,13 @@ $(document).ready(function(){
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: '.signature .next',
+		prevEl: '.signature .prev',
 	},
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-	},
+	// pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+	// 	el: '.swiper-pagination', /* 해당 요소의 class명 */
+	// 	clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+	// 	type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+	// },
     });
 })
