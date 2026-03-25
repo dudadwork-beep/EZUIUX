3f5c6b1342ccb02a39ac84369c473aa51af523bf
dudadwork-beep <dudad.work@gmail.com>
2026-03-25 17:22:24 +0900

    ..

--- happybakery/js/main.js
+++ happybakery/js/main.js
@@ -11,9 +11,49 @@ $(document).ready(function(){
             el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         },
-	
-
     });
+    /**** header ***/
+    let device_steus
+    let window_w
+    let mobile_size=1024
+    function device_chk(){
+        window_w = $(window).width()
+        if(window_w > mobile_size){
+            device_steus =  'pc'
+        }else{
+            device_steus =  'mo'
+        }
+        console.log(device_steus)
+    }
+    device_chk()
+    $(window).resize(function(){
+        device_chk()
+    })
+    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+        $(this).addClass('over')
+        $('.header').addClass('menu_over')
+    })
+     $('.header').on('mouseleave', function(){
+        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+        $('.header').removeClass('menu_over')
+    })
+
+    let scrolling
+    function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        // console.log(scrolling)
+        if(scrolling > 0){
+            $('.header').addClass('fixed')
+        }else{
+            $('.header').removeClass('fixed')
+        }
+    }
+    scroll_chk()
+    $(window).scroll(function(){
+        scroll_chk()
+    })
+
     /****************!!signature swiper!!**************** */
     const signature_swiper = new Swiper('.signature .swiper', { /* 팝업을 감싼는 요소의 class명 */
 	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
