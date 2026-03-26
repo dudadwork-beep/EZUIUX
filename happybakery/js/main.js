4cb001358846d03b38a342e76ea4d397f111865f
dudadwork-beep <dudad.work@gmail.com>
2026-03-26 15:41:50 +0900

    ..

--- happybakery/js/main.js
+++ happybakery/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     const visualt_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 2500,
-        //     disableOnInteraction: true,
-        // },
-        // effect: "fade", /* fade 효과 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+        effect: "fade", /* fade 효과 */
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
@@ -80,4 +80,11 @@ $(document).ready(function(){
 		prevEl: '.signature .prev',
 	}
     });
+    /***************Top버튼************ */
+    $('.footer .top').on('click', function(){
+        // console.log('클릭')
+        $('html,body').animate({
+            scrollTop: 0
+        },500)
+    })
 })
