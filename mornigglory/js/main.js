646c5e2aa4618e8679b8ed3e48126f9b5075b6da
dudadwork-beep <dudad.work@gmail.com>
2026-04-07 18:04:37 +0900

    ..

--- mornigglory/js/main.js
+++ mornigglory/js/main.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
-    const visualt_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
-            disableOnInteraction: true,
-        },
-        effect: "fade", /* fade 효과 */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.visual .swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-        },
-    });
-    /**** header ***/
+    console.log('look')
+/**** header ***/
     let device_steus
     let window_w
     let mobile_size=1024
@@ -29,62 +17,45 @@ $(document).ready(function(){
     $(window).resize(function(){
         device_chk()
     })
-    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
-        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+    $('.header .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
+        $('.header .gnb_wrap ul.depth1 > li').removeClass('over')
         $(this).addClass('over')
-        $('.header').addClass('menu_over')
     })
-     $('.header').on('mouseleave', function(){
-        $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
-        $('.header').removeClass('menu_over')
+     $('.header .gnb_wrap ul.depth1 > li').on('mouseleave', function(){
+        $('.header .gnb_wrap ul.depth1 > li').removeClass('over')
     })
-
-    let scrolling
-    function scroll_chk(){
-        scrolling = $(window).scrollTop()
-        // console.log(scrolling)
-        if(scrolling > 0){
-            $('.header').addClass('fixed')
-        }else{
-            $('.header').removeClass('fixed')
+/****************!!popup(비주얼) swiper!!**************** */
+   const popup_swiper = new Swiper('.popup .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 100,
+            },
+        },
+        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+        },
+        navigation: {
+            nextEl: '.popup .next',
+            prevEl: '.popup .prev',
         }
-    }
-    scroll_chk()
-    $(window).scroll(function(){
-        scroll_chk()
-    })
-
-    /****************!!signature swiper!!**************** */
-    const signature_swiper = new Swiper('.signature .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-	breakpoints: {
-		769: {    /* 640px 이상일때 적용 */
-			slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 20,
-		},
-		1370: {    /* 640px 이상일때 적용 */
-                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 24,
-		},
-		1381: {    /* 640px 이상일때 적용 */
-                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 24,
-		},
-	},
-	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-	navigation: {
-		nextEl: '.signature .next',
-		prevEl: '.signature .prev',
-	}
     });
-    /***************Top버튼************ */
-    $('.footer .top').on('click', function(){
-        // console.log('클릭')
-        $('html,body').animate({
-            scrollTop: 0
-        },500)
+     $('.popup .ctrl_wrap .stop').on('click', function(){
+        // console.log('정지버튼을 눌렀어요!!!!!!!!')
+        // stop 숨김 play 보임
+        popup_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.popup .ctrl_wrap .play').show()
+    })
+    $('.popup .ctrl_wrap .play').on('click', function(){
+        // console.log('재생버튼을 눌렀어요!!!!!!!!')
+         popup_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.popup .ctrl_wrap .stop').show()
     })
 })
